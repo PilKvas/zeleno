a8cd017c05992a39fe730aa172abacd5e6fbe184
DarbinianAM <DarbinianAM@suek.ru>
2026-04-04 22:54:35 +0300

    Рефакторинг регистрации и авторизации

---
+++ lib/features/profile/data/dto/user_dto.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserDtoImpl _$$UserDtoImplFromJson(Map<String, dynamic> json) =>
+    _$UserDtoImpl(
+      email: json['email'] as String,
+      username: json['username'] as String,
+      firstName: json['first_name'] as String?,
+      lastName: json['last_name'] as String?,
+      gender: $enumDecodeNullable(_$GenderEnumMap, json['gender']),
+      bio: json['bio'] as String?,
+      birthday: json['birthday'] == null
+          ? null
+          : DateTime.parse(json['birthday'] as String),
+      location: json['location'] as String?,
+      hemisphere: $enumDecodeNullable(_$HemisphereEnumMap, json['hemisphere']),
+    );
+
+Map<String, dynamic> _$$UserDtoImplToJson(_$UserDtoImpl instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'username': instance.username,
+      'first_name': instance.firstName,
+      'last_name': instance.lastName,
+      'gender': _$GenderEnumMap[instance.gender],
+      'bio': instance.bio,
+      'birthday': instance.birthday?.toIso8601String(),
+      'location': instance.location,
+      'hemisphere': _$HemisphereEnumMap[instance.hemisphere],
+    };
+
+const _$GenderEnumMap = {
+  Gender.male: 'male',
+  Gender.female: 'female',
+};
+
+const _$HemisphereEnumMap = {
+  Hemisphere.north: 'north',
+  Hemisphere.east: 'east',
+  Hemisphere.south: 'south',
+  Hemisphere.west: 'west',
+};
---
+++ lib/features/profile/presentation/cubit/profile_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:zeleno_v2/features/profile/domain/model/user.dart';
+import 'package:zeleno_v2/features/profile/domain/repository/i_profile_repository.dart';
+
+part 'profile_cubit.freezed.dart';
+part 'profile_state.dart';
+
+class ProfileCubit extends Cubit<ProfileState> {
+  ProfileCubit({required IProfileRepository profileRepository})
+      : _profileRepository = profileRepository,
+        super(const ProfileState.initial());
+
+  final IProfileRepository _profileRepository;
+
+  Future<void> loadUser() async {
+    emit(const ProfileState.loading());
+    try {
+      final user = await _profileRepository.getCurrentUser();
+      emit(ProfileState.loaded(user));
+    } catch (error, _) {
+      emit(ProfileState.failure(error));
+    }
+  }
+}
